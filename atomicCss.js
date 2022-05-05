0779ae0627301b33157a552116109dc0daf102f1
quandoan21-legion <qdoan21.legion@gmail.com>
2022-05-05 16:39:35 +0700

    commit test

--- atomicCss.js
+++ atomicCss.js
@@ -15,7 +15,7 @@ atomic.setConfig({
     "color-tertiary": "var(--color-tertiary)",
   },
 });
-//coder ngao 1
+//coder ngao 12
 atomic.customValue((value) => {
   const regexp = /\.\d*/g;
   if (regexp.test(value) && value.includes("color")) {
