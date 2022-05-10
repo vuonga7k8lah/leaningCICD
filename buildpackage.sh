a182d32528aa574e8a971242f35ba58e4b59814e
vuonga7k8lah <vuonga7k8lnc@gmail.com>
2022-05-10 09:38:23 +0700

    test-commit

--- buildpackage.sh
+++ buildpackage.sh
@@ -6,11 +6,12 @@ echo "Enter the SOURCE_DIR: "
 read SOURCE_DIR
 echo "Enter the TARGET_DIR: "
 read TARGET_DIR
-
-mv -f $SOURCE_DIR $TARGET_DIR \
+rm -rf $TARGET_DIR \
+&& mkdir -m 750 $TARGET_DIR \
+&& cp -af $SOURCE_DIR $TARGET_DIR \
 && git status \
 && git add . \
-&& git commit -m commitMessage \
+&& git commit -m $commitMessage \
 && git push -f
 
 echo "Build package is done"
